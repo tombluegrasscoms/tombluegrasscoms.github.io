0e13aaff166c78c5f9b3011787544e53d5180022
tombluegrasscoms <tom.small@bluegrasscoms.com>
2016-04-21 11:49:40 +0100

    *8
    **

---
+++ smoothtest/js/scripts.js
@@ -0,0 +1,35 @@
+var isMobile = {
+    Android: function() {
+        return navigator.userAgent.match(/Android/i);
+    },
+    BlackBerry: function() {
+        return navigator.userAgent.match(/BlackBerry/i);
+    },
+    iOS: function() {
+        return navigator.userAgent.match(/iPhone|iPad|iPod/i);
+    },
+    Opera: function() {
+        return navigator.userAgent.match(/Opera Mini/i);
+    },
+    Windows: function() {
+        return navigator.userAgent.match(/IEMobile/i);
+    },
+    any: function() {
+        return (isMobile.Android() || isMobile.BlackBerry() || isMobile.iOS() || isMobile.Opera() || isMobile.Windows());
+    }
+};
+if( !isMobile.any() )
+$(function(){
+    $.stellar({
+    horizontalScrolling: false,
+    verticalOffset: 150
+    });
+});
+var target = $('.title');
+var targetHeight = target.outerHeight();
+$(document).scroll(function(e){
+    var scrollPercent = (targetHeight - window.scrollY) / targetHeight;
+    if(scrollPercent >= 0){
+        target.css('opacity', scrollPercent);
+    }
+});
